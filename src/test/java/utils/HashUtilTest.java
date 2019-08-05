b832770396cbc9555116bf61553194d0bf1be86b
Igor Prykhodko <igorprihodkoemail@gmail.com>
2019-08-05 14:48:12 +0300

    Homework_11 CS fixes.

--- src/test/java/utils/HashUtilTest.java
+++ src/test/java/utils/HashUtilTest.java
@@ -5,50 +5,54 @@ import org.testng.annotations.Test;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
 
 public class HashUtilTest {
 
-    static String password;
+    private static String password;
 
     @Test(expectedExceptions = NullPointerException.class)
-    public void testGenerateHashPassword()
+    public void generateHashPasswordTestOne()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = null;
         assertNull(HashUtil.generateHashPassword(password));
     }
 
     @Test
-    public void testGenerateHashPassword1()
+    public void generateHashPasswordTestTwo()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = "123";
         assertNotEquals(null, HashUtil.generateHashPassword(password));
     }
 
     @Test
-    public void testGenerateHashPassword2()
+    public void generateHashPasswordTestThree()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = "3344";
         assertNotEquals("23333", HashUtil.generateHashPassword(password));
     }
 
     @Test(expectedExceptions = NullPointerException.class)
-    public void testValidatePassword()
+    public void validatePasswordTestOne()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = null;
         assertTrue(HashUtil.validatePassword(password, HashUtil.generateHashPassword(password)));
     }
 
     @Test
-    public void testValidatePassword1()
+    public void validatePasswordTestTwo()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = "4455";
         String actual = HashUtil.generateHashPassword(password);
-        assertNotEquals(actual, HashUtil.generateHashPassword(password));
+        String expected = HashUtil.generateHashPassword(password);
+        assertNotEquals(actual, expected);
     }
 
     @Test
-    public void testValidatePassword2()
+    public void validatePasswordTestThree()
             throws InvalidKeySpecException, NoSuchAlgorithmException {
         password = "4455";
         String storedPassword = HashUtil.generateHashPassword(password);
